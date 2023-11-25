c7aaf5e6f7ee34f94d26a6fcda6b05e8247eb561
matsubrasil <matsuboy@outlook.com>
2023-11-25 10:17:42 -0300

    refactor: statusMap

--- src/components/ui/IssueStatusBadge.tsx
+++ src/components/ui/IssueStatusBadge.tsx
@@ -4,14 +4,19 @@ import { Badge } from '@radix-ui/themes'
 interface IssueStatusBadgeProps {
   status: Status
 }
+
 const statusMap: Record<
   Status,
-  { label: string; color: 'red' | 'violet' | 'green' }
+  {
+    label: string
+    color: 'red' | 'violet' | 'green'
+  }
 > = {
   OPEN: { label: 'Open', color: 'red' },
   IN_PROGRESS: { label: 'In Progress', color: 'violet' },
   CLOSED: { label: 'Closed', color: 'green' },
 }
+
 export function IssueStatusBadge({ status }: IssueStatusBadgeProps) {
   return (
     <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
